949022846f808db10dd03150050e4fa05abeba27
techiee619 <brainlytech@gmail.com>
2023-02-27 16:24:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import "./App.css";
+import { Link } from "react-scroll";
+
+function App() {
+  const menuItems = [
+    {
+      id: 1,
+      title: "home",
+    },
+    {
+      id: 2,
+      title: "portfolio",
+    },
+    {
+      id: 3,
+      title: "contact",
+    },
+    {
+      id: 4,
+      title: "about",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <h2>Coffee Shop</h2>
+
+          <ul>
+            {menuItems.map((menu) => (
+              <li>
+                <Link
+                  to={menu.title}
+                  smooth={true}
+                  offset={-430}
+                  duration={500}
+                >
+                  {menu.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        {menuItems.map((menu) => (
+          <div className="content">
+            <h1 className="content-header" id={menu.title}>
+              {menu.title}
+            </h1>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
+
+export default App;
